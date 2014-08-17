bc49a49f5774770e506b6845e1257d2bc55b7d72
Taru Karttunen <taruti@taruti.net>
2014-08-17 16:42:19 +0300

    Return empty words for punctuation

--- word_scanners.go
+++ word_scanners.go
@@ -2,41 +2,44 @@
 package langword
 
 import (
+	"code.google.com/p/go.text/unicode/norm"
 	"unicode"
 	"unicode/utf8"
-	"code.google.com/p/go.text/unicode/norm"
 )
 
 // Scan words in latin alphabets, suitable for bufio.Scanner. Returns lowercase words containing only
 // letters that are unicode normalized (NFC).
 func ScanLatinWords(data []byte, atEOF bool) (advance int, token []byte, err error) {
-        // Skip leading spaces.
-        start := 0
-        for width := 0; start < len(data); start += width {
-                var r rune
-                r, width = utf8.DecodeRune(data[start:])
-                if unicode.IsLetter(r) {
-                        break
-                }
-        }
-        if atEOF && len(data) == 0 {
-                return 0, nil, nil
-        }
-        // Scan until space, marking end of word.
-        for width, i := 0, start; i < len(data); i += width {
-                var r rune
-                r, width = utf8.DecodeRune(data[i:])
-		
-			if !unicode.IsLetter(r) {
-				return i + width, mutateWord(data[start:i]), nil
-			} 
-       }
-        // If we're at EOF, we have a final, non-empty, non-terminated word. Return it.
-        if atEOF && len(data) > start {
-			return len(data), mutateWord(data[start:]), nil
-        }
-        // Request more data.
-        return 0, nil, nil
+	// Skip leading spaces.
+	start := 0
+	for width := 0; start < len(data); start += width {
+		var r rune
+		r, width = utf8.DecodeRune(data[start:])
+		if unicode.IsLetter(r) {
+			break
+		}
+		if unicode.IsPunct(r) {
+			return start + width, nil, nil
+		}
+	}
+	if atEOF && len(data) == 0 {
+		return 0, nil, nil
+	}
+	// Scan until space, marking end of word.
+	for width, i := 0, start; i < len(data); i += width {
+		var r rune
+		r, width = utf8.DecodeRune(data[i:])
+
+		if !unicode.IsLetter(r) {
+			return i + width, mutateWord(data[start:i]), nil
+		}
+	}
+	// If we're at EOF, we have a final, non-empty, non-terminated word. Return it.
+	if atEOF && len(data) > start {
+		return len(data), mutateWord(data[start:]), nil
+	}
+	// Request more data.
+	return 0, nil, nil
 }
 
 func mutateWord(bs []byte) []byte {
@@ -45,11 +48,11 @@ func mutateWord(bs []byte) []byte {
 
 func toLowerBytes(src []byte) []byte {
 	dst := make([]byte, 0, len(src))
-	for width, i, di := 0,0,0; i<len(src); i+= width {
+	for width, i, di := 0, 0, 0; i < len(src); i += width {
 		var r rune
 		r, width = utf8.DecodeRune(src[i:])
 		r = unicode.ToLower(r)
-		dw:=utf8.RuneLen(r)
+		dw := utf8.RuneLen(r)
 		dst = append(dst, make([]byte, dw)...)
 		utf8.EncodeRune(dst[di:], r)
 		di += dw
